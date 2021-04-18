8c8c3d5e072d00981fce889628dc3f4a49d9916d
George <george@gplus.gr>
2021-04-18 21:51:47 +0300

    Fixing Component Structure of Search form and Weather forecast

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import SearchForm from "./SearchForm";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import WeatherCard from "./WeatherCard";
 import ForeCastCard from "./ForeCastCard";
 import Footer from "./Footer";
@@ -7,6 +7,40 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 export default function App() {
+  let [city, setCity] = useState("London");
+  let [weather, setWeather] = useState({});
+
+  // // Similar to componentDidMount and componentDidUpdate:
+  // useEffect(() => {
+  //   // Update the document title using the browser API
+  //   getWeather();
+  // }, [city, getWeather]);
+
+  function getWeather() {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c0f3afe2be69a14ab9fb1d02ca6c2d47&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    getWeather();
+  }
+
+  function handleChange(event) {
+    setCity(event.target.value);
+  }
+
+  function handleResponse(response) {
+    const iconCode = response.data.weather[0].icon;
+    setWeather({
+      iconSource: `https://openweathermap.org/img/wn/${iconCode}@2x.png`,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
+
   return (
     <div className="App">
       <div className="container font-monospace">
@@ -16,19 +50,41 @@ export default function App() {
               <div className="col-md-12">
                 <div className="row">
                   <div className="col-md-12">
-                    <SearchForm />
+                    <div className="SearchForm">
+                      <form onSubmit={handleSubmit} className="search-form">
+                        <div className="input-group mb-3">
+                          <input
+                            type="search"
+                            className="form-control search-location"
+                            placeholder="Search Location"
+                            onChange={handleChange}
+                            aria-label="Search Location"
+                            aria-describedby="button-addon2"
+                          />
+                          <button
+                            className="btn btn-outline-secondary find-city"
+                            type="submit"
+                            value="submit"
+                          >
+                            <i className="fas fa-search"></i>
+                          </button>
+                          <button
+                            className="btn btn-outline-secondary pin-button"
+                            type="button"
+                          >
+                            <i className="fas fa-map-pin"></i>
+                          </button>
+                        </div>
+                      </form>
+                    </div>
                   </div>
                 </div>
                 <div className="row mt-5">
                   <div className="col-md-12">
                     <WeatherCard
-                      city="Paris"
+                      city={city}
                       date="Wednesday 14 April"
-                      temperature={20}
-                      humidity={90}
-                      wind={5}
-                      description="Clear
-                      Sky"
+                      weather={weather}
                     />
                   </div>
                 </div>
--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,36 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import "./WeatherCard.css";
+import moment from "moment";
 
 export default function WeatherCard(props) {
-  let [city, setCity] = useState("props.city");
-  let [temperature, setTemperature] = useState(props.temperature);
-
-  function farheneit(event) {
-    event.preventDefault();
-    setTemperature(Math.round((props.temperature * 9) / 5 + 32));
-  }
-  function celsius(event) {
-    event.preventDefault();
-    setTemperature(props.temperature);
-  }
-
   return (
     <div className="card weather-card">
       <div className="card-body border border-5 rounded">
         <p className="card-text display-5 cities">{props.city}</p>
-        <p className="card-text current-date">{props.date}</p>
+        <p className="card-text current-date">
+          {moment().format("MMMM Do YYYY, h:mm:ss a")}
+        </p>
         <div className="row">
           <div className="col-md-6">
             <div className="creafix main-weather">
-              <span className="float-left weatherIcon"></span>
+              <span className="float-left weatherIcon">
+                <img src={props.weather.iconSource} alt="weather icon" />
+              </span>
               <div className="float-left">
                 <span className="temperature celsius actualTemp">
-                  {props.temperature}
+                  {props.weather.temperature}
                 </span>
                 <span className="buttons">
                   <a
                     href="/"
-                    onClick={celsius}
+                    onClick={null}
                     className="js-toggle-unit js-celsius active"
                   >
                     °C
@@ -38,7 +31,7 @@ export default function WeatherCard(props) {
                   |{" "}
                   <a
                     href="/"
-                    onClick={farheneit}
+                    onClick={null}
                     className="js-toggle-unit js-fahrenheit"
                   >
                     °F
@@ -51,20 +44,18 @@ export default function WeatherCard(props) {
           <div className="col-md-6">
             <div className="weather-details">
               <div>
-                Pressure:
-                <span className="pressure">5</span>
-              </div>
-              <div>
                 Wind:
-                <span className="wind-speed">{props.wind}</span>
+                <span className="wind-speed">{props.weather.wind}</span>
               </div>
               <div>
                 Humidity:
-                <span className="humidity">{props.humidity}%</span>
+                <span className="humidity">{props.weather.humidity}%</span>
               </div>
             </div>
           </div>
-          <p className="card-text weather-description">{props.description}</p>
+          <p className="card-text weather-description">
+            {props.weather.description}
+          </p>
         </div>
       </div>
     </div>
